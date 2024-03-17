163f47122c09138d961904be2daf3d767c357df1
gyangho <52252845+gyangho@users.noreply.github.com>
2024-03-17 17:45:07 +0900

    2024.03.17. 99퍼 틀림...

--- 1747/1747.cpp
+++ 1747/1747.cpp
@@ -9,28 +9,31 @@ int main()
 {
   std::cin.tie(NULL);
   std::ios::sync_with_stdio(false);
-  std::cout << "hello";
   int N;
   int iAns;
+
   std::cin >> N;
   iAns = N;
   while (1)
   {
     if (IsPalindrome(iAns) && IsPrime(iAns))
+    {
       std::cout << iAns;
+      break;
+    }
+    iAns += 1;
   }
-
   return 0;
 }
 
 int IsPrime(int n)
 {
-  int iRet = 0;
+  int iRet = 1;
   for (int i = 2; i < n; i++)
   {
     if (n % i == 0)
     {
-      iRet = 1;
+      iRet = 0;
       break;
     }
   }
@@ -39,24 +42,25 @@ int IsPrime(int n)
 
 int IsPalindrome(int n)
 {
-  return (Reverse(n) == n);
+  if (Reverse(n) == n)
+    return 1;
+  else
+    return 0;
 }
 
 int Reverse(int n)
 {
   int iRet = 0;
   int iTemp = -1;
-  int iTens = 10;
   while (1)
   {
-    iTemp = n % iTens;
-    if (iTemp = 0)
+    if (n <= 0)
     {
       break;
     }
+    iTemp = n % 10;
     iRet = iRet * 10 + iTemp;
-    iTens *= 10;
-    std::cout << iRet << " ";
+    n = n / 10;
   }
   return iRet;
 }
